cb0a7b70ea089b2a52157aa8ff65d7784bafdcaf
smarriagac <smarriagac@gmail.com>
2023-03-20 23:06:40 -0500

    Style: agregando stilos navbar

--- lib/app/presentation/modules/home/controller/home_controller.dart
+++ lib/app/presentation/modules/home/controller/home_controller.dart
@@ -1,3 +1,20 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final homeProvider = StateProvider<int>((ref) => 0);
+
+final homeScrollController = ChangeNotifierProvider<ScrollController>(
+  (ref) => ScrollController(),
+);
+
+class ScrollController extends ChangeNotifier {
+  final homeviewController = PageController();
+
+  void onScrollViewPage(int i) {
+    homeviewController.animateToPage(
+      i,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInOut,
+    );
+  }
+}
--- lib/app/presentation/modules/home/view/home_view.dart
+++ lib/app/presentation/modules/home/view/home_view.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+import '../../../global/extension/build_context_ext.dart';
 import '../controller/home_controller.dart';
 
 class HomeView extends ConsumerWidget {
@@ -16,21 +17,84 @@ class HomeView extends ConsumerWidget {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(
+          const Positioned(
             left: 0,
             right: 0,
-            child: Container(
-              height: kToolbarHeight,
-              color: Colors.red,
-            ),
+            child: NavBar(),
           ),
-          Center(
-            child: Text('HOLA CONTADOR ${ref.watch(homeProvider)}'),
+          Positioned(
+            top: kToolbarHeight,
+            height: context.hg - kToolbarHeight,
+            width: context.wd,
+            child: const BodyHome(),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: () => ref.read(homeProvider.notifier).state++),
+    );
+  }
+}
+
+class BodyHome extends ConsumerWidget {
+  const BodyHome({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final scrollController = ref.read(homeScrollController).homeviewController;
+
+    return PageView(
+      controller: scrollController,
+      physics:
+          const BouncingScrollPhysics(parent: AlwaysScrollableScrollPhysics()),
+      scrollDirection: Axis.vertical,
+      onPageChanged: (value) => log('$value'),
+      children: [
+        Container(color: Colors.green),
+        Container(color: Colors.orange),
+        Container(color: Colors.yellow),
+        Container(color: Colors.black26),
+      ],
+    );
+  }
+}
+
+class NavBar extends ConsumerWidget {
+  const NavBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final controller = ref.read(homeScrollController);
+
+    return SizedBox(
+      height: kToolbarHeight,
+      // color: Colors.red,
+      width: double.maxFinite,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          MouseRegion(
+            cursor: MouseCursor.uncontrolled,
+            onHover: (event) {
+              if (event.distanceMax.isFinite) {
+                controller.onScrollViewPage(1);
+              }
+            },
+            child: const Text('Inicio'),
+          ),
+          const SizedBox(width: 20),
+          const Text('Acerca de mi'),
+          const SizedBox(width: 20),
+          const Text('Portafolio'),
+          const SizedBox(width: 20),
+          const Text('Contacto'),
+          const SizedBox(width: 20),
+        ],
+      ),
     );
   }
 }
