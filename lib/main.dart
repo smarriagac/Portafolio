760959bfe885f0474a86ddc3d80e097b04e70487
Sebastian marriaga <sebastian.marriaga@silvercredit.co>
2023-03-21 17:00:39 -0500

    Fix: correcion en traduccion

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'app/my_app.dart';
 void main() {
   setPathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
-  LocaleSettings.setLocale(AppLocale.en);
+  LocaleSettings.setLocale(AppLocale.es);
   runApp(
     ProviderScope(
       child: TranslationProvider(
